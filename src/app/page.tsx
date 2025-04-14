c45c941a2d2617329d7f92d0b699166db817aac9
pietrodev07 <pietro.dev.07@gmail.com>
2025-04-14 19:18:41 +0200

    chore: change the main title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default async function Home() {
   return (
     <main className="h-svh flex flex-col gap-6 items-center justify-center">
-      <h1 className="font-semibold text-4xl">pietrodev07&apos;s website</h1>
+      <h1 className="font-semibold text-4xl">pietrodev07 website</h1>
       <Button variant="outline">let&apos;s ship it soon pleaseeeee</Button>
     </main>
   );
