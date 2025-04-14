ffa57d8de8c2b855b5cefa0c127f9fd993d81e7b
pietrodev07 <pietro.dev.07@gmail.com>
2025-04-14 19:13:24 +0200

    chore: change some texts in the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,10 @@
 import { Button } from "@/components/ui/button";
 
-const sampleAction = async () => {
-  "use server";
-  console.log("throw an action");
-};
-
 export default async function Home() {
   return (
     <main className="h-svh flex flex-col gap-6 items-center justify-center">
-      <h1 className="font-semibold text-4xl">Nextjs Clean Starter</h1>
-
-      <form action={sampleAction}>
-        <Button className="cursor-pointer" variant="outline">
-          click me to throw something or not
-        </Button>
-      </form>
+      <h1 className="font-semibold text-4xl">pietrodev07&apos;s website</h1>
+      <Button variant="outline">let&apos;s ship it soon pleaseeeee</Button>
     </main>
   );
 }
