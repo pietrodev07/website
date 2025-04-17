78d5d1df128e1c27e11b7199034fa29632fcc935
pietrodev07 <pietro.dev.07@gmail.com>
2025-04-17 09:39:52 +0200

    chore: add open-graph metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,16 +2,14 @@ import "./globals.css";
 import { Metadata } from "next";
 import { cn } from "@/lib/utils";
 import { PropsWithChildren } from "react";
+import { metadataLib } from "@/lib/metadata";
 import { Providers } from "@/components/providers";
 import { Bricolage_Grotesque } from "next/font/google";
 import { ThemeToggle } from "@/components/theme-toggle";
 
 const bricolageFont = Bricolage_Grotesque({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "pietrodev07",
-  description: "pietrodev07's personal website",
-};
+export const metadata: Metadata = metadataLib;
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
---
+++ src/lib/metadata.ts
@@ -0,0 +1,8 @@
+import { Metadata } from "next";
+
+export const metadataLib: Metadata = {
+  title: "pietrodev07",
+  description: "pietrodev07's personal website",
+  twitter: { images: [{ url: "./og.png" }] },
+  openGraph: { images: [{ url: "./og.png" }] },
+};
